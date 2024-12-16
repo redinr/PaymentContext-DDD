246a50f573669e54b1662c3a4a5c1a79526abaa8
Ricardo Redin <ricardoredin07@gmail.com>
2024-12-16 14:49:41 -0300

    Implements ideia Repositories

--- PaymentContext.Domain/obj/Debug/net8.0/PaymentContext.Domain.AssemblyInfo.cs
+++ PaymentContext.Domain/obj/Debug/net8.0/PaymentContext.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentContext.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe6082e6b9513e770fc2565a34650b5753894a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8245f3fdf4f104a2553113356b4373d46a90143")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentContext.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentContext.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
